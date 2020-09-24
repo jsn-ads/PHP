2be13534af8dd44d5423508502b9083039f22f80
neto <jsn.ads@gmail.com>
2020-09-24 09:34:36 -0300

    Clone do google finalizado

---
+++ 02-HTML5_CSS3/25 Clone do google/assets/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#text1').mouseover(function(){
+        $('#text1').css("text-decoration","underline");
+    });
+    $('#text1').mouseout(function(){
+        $('#text1').css("text-decoration","none");
+    });
+
+    $('#text2').mouseover(function(){
+        $('#text2').css("text-decoration","underline");
+    });
+    $('#text2').mouseout(function(){
+        $('#text2').css("text-decoration","none");
+    });
+})
---
+++ 02-HTML5_CSS3/25 Clone do google/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicial</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+    
+        <div class="top">
+            <div class="text" id="text1">
+                Gmail
+            </div>
+            <div class="text" id="text2">
+                Imagens
+            </div>
+            <img class="apps" src="assets/img/menu.png">
+            <div class="login">
+                <strong>G</strong>
+            </div>
+        </div>
+
+        <div class="session">
+            <img class="titulo" src="assets/img/google.png" alt="">
+            <div class="pesquisa">
+                <div class="form-group">
+                    <input class="form-control" type="text" id="ps">
+                </div>
+            </div>
+            <div class="bt1">
+                <button id="btn1" class="btn1 btn btn-light">Pesquisa Google</button>
+                <button id="btn2" class="btn2 btn btn-light">Estou com Sorte</button>
+            </div>
+        </div>
+
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+</html>
